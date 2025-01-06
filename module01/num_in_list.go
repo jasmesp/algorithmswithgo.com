1c7960e5c579cb25bf49a08165777436f8f74321
jasmesp <jasmes@gmail.com>
2025-01-05 23:56:58 -0500

    Enhance NumInList by adding type checking and logging for type mismatches

--- module01/num_in_list.go
+++ module01/num_in_list.go
@@ -1,7 +1,24 @@
 package module01
 
+import (
+	"fmt"
+	"reflect"
+)
+
 // NumInList will return true if num is in the
 // list slice, and false otherwise.
 func NumInList(list []int, num int) bool {
+	for _, i := range list {
+		if i == num {
+			return true
+		} else {
+			if reflect.TypeOf(i) != reflect.TypeOf(num) {
+				fmt.Println("Type mismatch")
+				return false
+			}
+
+		}
+
+	}
 	return false
 }
